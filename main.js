9000eeedf1de1f80c54445ecc8f1b126e47c7d5a
Alankord <147655822+Alankord@users.noreply.github.com>
2026-08-06 14:46:33 +0300

    Implement auto-updater with dialog on update
    Added auto-update functionality with user notification.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, BrowserWindow, Menu, dialog } = require('electron');
 const path = require('path');
+const { autoUpdater } = require('electron-updater');
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -16,14 +17,27 @@ function createWindow() {
   });
 
   Menu.setApplicationMenu(null);
+  win.setFullScreen(true);
   win.loadFile('index.html');
 }
 
 app.whenReady().then(() => {
   createWindow();
+  autoUpdater.checkForUpdatesAndNotify();
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+autoUpdater.on('update-downloaded', () => {
+  dialog.showMessageBox({
+    type: 'info',
+    title: 'وەشانی نوێ',
+    message: 'وەشانێکی نوێ دابەزێنرا. پرۆگرامەکە دادەخرێتەوە بۆ دامەزراندنی نوێکردنەوەکە.',
+    buttons: ['باشە']
+  }).then(() => {
+    autoUpdater.quitAndInstall();
   });
 });
 
